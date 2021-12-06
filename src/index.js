93d4a9b9b5ee19827f60c2bc3db95c525e5d3b20
Julian Smith <jsmith2@g2.com>
2021-12-06 17:14:18 -0600

    initialized redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import "./styles/main.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import { createStore } from "redux";
+import rootReducer from "./reducers/index";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
