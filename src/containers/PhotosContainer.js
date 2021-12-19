f16c9b2d6091ec51a12ba5dc5c6938a855e0643c
Julian Antonio Smith <juliansmith94@hotmail.com>
2021-12-18 19:22:17 -0600

    added alt attributes photos

--- src/containers/PhotosContainer.js
+++ src/containers/PhotosContainer.js
@@ -4,24 +4,24 @@ const PhotosContainer = (props) => {
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-3 gap-1 md:gap-7">
-        <img src={"https://picsum.photos/id/23/300"} />
-        <img src={"https://picsum.photos/id/24/300"} />
-        <img src={"https://picsum.photos/id/25/300"} />
-        <img src={"https://picsum.photos/id/26/300"} />
-        <img src={"https://picsum.photos/id/27/300"} />
-        <img src={"https://picsum.photos/id/28/300"} />
-        <img src={"https://picsum.photos/id/29/300"} />
-        <img src={"https://picsum.photos/id/30/300"} />
-        <img src={"https://picsum.photos/id/31/300"} />
-        <img src={"https://picsum.photos/id/23/300"} />
-        <img src={"https://picsum.photos/id/24/300"} />
-        <img src={"https://picsum.photos/id/25/300"} />
-        <img src={"https://picsum.photos/id/26/300"} />
-        <img src={"https://picsum.photos/id/27/300"} />
-        <img src={"https://picsum.photos/id/28/300"} />
-        <img src={"https://picsum.photos/id/29/300"} />
-        <img src={"https://picsum.photos/id/30/300"} />
-        <img src={"https://picsum.photos/id/31/300"} />
+        <img alt="" src={"https://picsum.photos/id/23/300"} />
+        <img alt="" src={"https://picsum.photos/id/24/300"} />
+        <img alt="" src={"https://picsum.photos/id/25/300"} />
+        <img alt="" src={"https://picsum.photos/id/26/300"} />
+        <img alt="" src={"https://picsum.photos/id/27/300"} />
+        <img alt="" src={"https://picsum.photos/id/28/300"} />
+        <img alt="" src={"https://picsum.photos/id/29/300"} />
+        <img alt="" src={"https://picsum.photos/id/30/300"} />
+        <img alt="" src={"https://picsum.photos/id/31/300"} />
+        <img alt="" src={"https://picsum.photos/id/23/300"} />
+        <img alt="" src={"https://picsum.photos/id/24/300"} />
+        <img alt="" src={"https://picsum.photos/id/25/300"} />
+        <img alt="" src={"https://picsum.photos/id/26/300"} />
+        <img alt="" src={"https://picsum.photos/id/27/300"} />
+        <img alt="" src={"https://picsum.photos/id/28/300"} />
+        <img alt="" src={"https://picsum.photos/id/29/300"} />
+        <img alt="" src={"https://picsum.photos/id/30/300"} />
+        <img alt="" src={"https://picsum.photos/id/31/300"} />
       </div>
     </div>
   );
